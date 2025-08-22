f98e4cb5b0c3cd39a3a25796a2333ba509a6492a
dmitray27 <dmitrayketz@gmail.com>
2025-08-22 21:10:41 +0300

    Fix Android Kotlin configuration

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,8 +7,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.4.0" apply false
+    id "dev.flutter.flutter-gradle-plugin" version "1.0.0" apply false
 }
 
-include(":app")
+include ":app"
