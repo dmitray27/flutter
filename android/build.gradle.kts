e046ad5569904108be09651c3a4f44e5e07532cb
dmitray27 <dmitrayketz@gmail.com>
2025-08-22 16:50:26 +0300

    Fix Android build configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,6 +8,11 @@ allprojects {
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
+// android/build.gradle.kts
+plugins {
+    kotlin("jvm") version "2.1.0"  // ← Обновите версию
+}
+
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
